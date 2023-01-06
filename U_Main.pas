d78adbea0866ac612bff2dcaebf38e2865e798cc
Hamza Mansouri <eHamzaDZ@protonmail.com>
2023-01-07 00:18:06 +0100

    New Feature - Profile button with menu

--- U_Main.pas
+++ U_Main.pas
@@ -5,7 +5,7 @@ interface
 uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
   FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.Objects,
-  FMX.Ani, FMX.Controls.Presentation, FMX.StdCtrls;
+  FMX.Ani, FMX.Controls.Presentation, FMX.StdCtrls, FMX.TabControl, FMX.Effects;
 
 type
   Tfrm_main = class(TForm)
@@ -35,6 +35,36 @@ type
     ColorAnimation9: TColorAnimation;
     Gradient_Light_Blue: TBrushObject;
     Gradient_Dark_Blue: TBrushObject;
+    content_area: TRectangle;
+    TabControl1: TTabControl;
+    tab_dashboard: TTabItem;
+    Rectangle2: TRectangle;
+    Circle2: TCircle;
+    rect_profile_bar: TRectangle;
+    Text1: TText;
+    Text2: TText;
+    ColorAnimation1: TColorAnimation;
+    Rectangle3: TRectangle;
+    BG: TBrushObject;
+    rect_profile_bar_menu: TRectangle;
+    Rectangle4: TRectangle;
+    ShadowEffect1: TShadowEffect;
+    Rectangle5: TRectangle;
+    Image1: TImage;
+    Text3: TText;
+    ColorAnimation2: TColorAnimation;
+    Rectangle6: TRectangle;
+    Image2: TImage;
+    Text5: TText;
+    ColorAnimation7: TColorAnimation;
+    Rectangle8: TRectangle;
+    Image4: TImage;
+    Text6: TText;
+    ColorAnimation8: TColorAnimation;
+    Rectangle9: TRectangle;
+    Image5: TImage;
+    Text7: TText;
+    ColorAnimation10: TColorAnimation;
     procedure Rect_dashboardClick(Sender: TObject);
     procedure Rect_patientsClick(Sender: TObject);
     procedure Rect_usersClick(Sender: TObject);
@@ -42,6 +72,7 @@ type
     procedure FormShow(Sender: TObject);
     procedure nav_barMouseDown(Sender: TObject; Button: TMouseButton;
       Shift: TShiftState; X, Y: Single);
+    procedure rect_profile_barClick(Sender: TObject);
   private
     { Private declarations }
   public
@@ -82,7 +113,7 @@ var
 begin
   this := TButton(sender);
   current_tab.Parent := this;
-  img_dashboard.Opacity := 1;
+  img_dashboard.Opacity := 0.8;
   img_patients.Opacity := 0.5;
   img_users.Opacity := 0.5;
   //tabcontrol1.TabIndex := 3;
@@ -95,8 +126,14 @@ begin
   this := TButton(sender);
   current_tab.Parent := this;
   img_dashboard.Opacity := 0.5;
-  img_patients.Opacity := 1;
+  img_patients.Opacity := 0.8;
   img_users.Opacity := 0.5;
+end;
+
+procedure Tfrm_main.rect_profile_barClick(Sender: TObject);
+begin
+  if rect_profile_bar_menu.Visible = false then rect_profile_bar_menu.Visible := true
+  else rect_profile_bar_menu.Visible := false;
 end;
 
 procedure Tfrm_main.Rect_usersClick(Sender: TObject);
@@ -107,7 +144,7 @@ begin
   current_tab.Parent := this;
   img_dashboard.Opacity := 0.5;
   img_patients.Opacity := 0.5;
-  img_users.Opacity := 1;
+  img_users.Opacity := 0.8;
 end;
 
 end.
